bd6c8907a1754fbc7ea494301d4acb4cdafdc7ed
Mahi-4 <sutharmaheshwari4215@gmail.com>
2025-12-19 20:39:07 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,72 @@
+let response;
+let data;
+
+function loadpage(page) {
+
+  const container = document.getElementById("main-container");
+  if (!container) {
+    console.error("main-container not found in DOM");
+    return;
+  }
+
+  console.log("loading page", page)
+
+  fetch(page)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Page not found" + response.status);
+      }
+      return response.text()
+    })
+    .then(data => {
+      container.innerHTML = data;
+    })
+    .catch(error => {
+      container.innerHTML =
+        "<h2>Error</h2><p>Content could not be loaded.</p>";
+      console.log("error", error)
+    });
+}
+
+
+
+$(document).ready(function () {
+  `~`
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    lazyLoad: true,
+    responsive: {
+      0: {           
+        items: 1,
+        margin: 10,
+        stagePadding: 20
+      },
+      576: {          
+        items: 1,
+        margin: 15
+      },
+      768: {         
+        items: 2
+      },
+      992: {         
+        items: 3
+      }
+    }
+  });
+});
+
+const handleClick = () => {
+  swal({
+    title: "Thankyou For Contacting Us!!!!",
+    text: "We Will Get Back To You Soon.",
+    icon: "success",
+    button: true,
+    timer: 3000,
+  });
+}
